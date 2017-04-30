e1ef27a9984d2515732c5a1be5ef9c38cd56b0c2
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-04-30 11:35:23 +0100

    feat: add a scriptTag option to allow non library targets
    Closes #3

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,53 +6,67 @@ const expect = require('chai').expect;
 const webpackRxjsExternals = require('../index');
 
 const fixturesDir = 'fixtures';
-const fixtures = fs.readdirSync(path.join(__dirname, fixturesDir));
+
+function testDir(fixturesSubDir, webpackOutputOptions, libOptions) {
+
+  const specFeaturesDir = path.join(fixturesDir, fixturesSubDir);
+
+  const fixtures = fs.readdirSync(path.join(__dirname, specFeaturesDir));
+
+  const promises = fixtures.map(fixture => {
+
+    return new Promise((resolve, reject) => {
+
+      try {
+
+        const config = {
+          entry: path.join(__dirname, specFeaturesDir, fixture, 'index.js'),
+          output: Object.assign({
+            filename: 'index.js',
+            path: __dirname
+          }, webpackOutputOptions),
+          externals: webpackRxjsExternals(libOptions)
+        };
+
+        const expectedOutput = fs.readFileSync(path.join(__dirname, specFeaturesDir, fixture, 'expected', 'index.js')).toString();
+
+        const compiler = webpack(config);
+        compiler.outputFileSystem = new MemoryFileSystem();
+
+        compiler.run((err, stats) => {
+          if (err) {
+            reject(err);
+          }
+          const actualOutput = stats.compilation.assets['index.js'].source();
+          expect(actualOutput).to.equal(expectedOutput);
+          resolve();
+        });
+
+      } catch (err) {
+        reject(err);
+      }
+
+    });
+
+  });
+
+  return Promise.all(promises);
+
+}
 
 describe('webpack-rxjs-externals', () => {
 
   it('should mark rxjs imports as external', () => {
-
-    const promises = fixtures.map(fixture => {
-
-      return new Promise((resolve, reject) => {
-
-        try {
-
-          const config = {
-            entry: path.join(__dirname, fixturesDir, fixture, 'index.js'),
-            output: {
-              filename: 'index.js',
-              path: __dirname,
-              libraryTarget: 'umd',
-              library: 'rxjsTest'
-            },
-            externals: webpackRxjsExternals()
-          };
-
-          const expectedOutput = fs.readFileSync(path.join(__dirname, fixturesDir, fixture, 'expected', 'index.js')).toString();
-
-          const compiler = webpack(config);
-          compiler.outputFileSystem = new MemoryFileSystem();
-
-          compiler.run((err, stats) => {
-            if (err) {
-              reject(err);
-            }
-            const actualOutput = stats.compilation.assets['index.js'].source();
-            expect(actualOutput).to.equal(expectedOutput);
-            resolve();
-          });
-
-        } catch (err) {
-          reject(err);
-        }
-
-      });
-
+    return testDir('default', {
+      libraryTarget: 'umd',
+      library: 'rxjsTest'
     });
+  });
 
-    return Promise.all(promises);
-
+  it('should allow rxjs to be included via a script tag', () => {
+    return testDir('script-tag', {}, {
+      scriptTag: true
+    });
   });
 
 });
