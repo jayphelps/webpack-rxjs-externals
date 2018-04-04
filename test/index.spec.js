9e0ea872ace18cd63a191d6b43157b7af81d2ceb
Jay Phelps <hello@jayphelps.com>
2018-04-04 00:06:35 -0400

    fix(v6): test that we don't add externals for packages that happen to have rxjs in their name

--- test/index.spec.js
+++ test/index.spec.js
@@ -10,50 +10,53 @@ const fixtures = fs.readdirSync(path.join(__dirname, fixturesDir))
   .filter(item => !(/(^|\/)\.[^\/\.]/g).test(item)); // ignore hidden files
 
 describe('webpack-rxjs-externals', () => {
-
   it('should mark rxjs imports as external', () => {
-
     const promises = fixtures.map(fixture => {
-
       return new Promise((resolve, reject) => {
+        const config = {
+          entry: path.join(__dirname, fixturesDir, fixture, 'index.js'),
+          output: {
+            filename: 'index.js',
+            path: __dirname,
+            libraryTarget: 'umd',
+            library: 'rxjsTest'
+          },
+          externals: [
+            webpackRxjsExternals(),
+            (context, request, callback) => {
+              const externs = ['whatever-rxjs', 'rxjs-whatever'];
+              const name = externs.find(item => item === request);
 
-        try {
-
-          const config = {
-            entry: path.join(__dirname, fixturesDir, fixture, 'index.js'),
-            output: {
-              filename: 'index.js',
-              path: __dirname,
-              libraryTarget: 'umd',
-              library: 'rxjsTest'
-            },
-            externals: webpackRxjsExternals()
-          };
-
-          const expectedOutput = fs.readFileSync(path.join(__dirname, fixturesDir, fixture, 'expected', 'index.js')).toString();
-
-          const compiler = webpack(config);
-          compiler.outputFileSystem = new MemoryFileSystem();
-
-          compiler.run((err, stats) => {
-            if (err) {
-              reject(err);
+              if (name) {
+                callback(null, {
+                  root: name,
+                  commonjs: name,
+                  commonjs2: name,
+                  amd: name.split('-').join('_')
+                });
+              } else {
+                callback();
+              }
             }
-            const actualOutput = stats.compilation.assets['index.js'].source();
-            expect(actualOutput).to.equal(expectedOutput);
-            resolve();
-          });
+          ]
+        };
 
-        } catch (err) {
-          reject(err);
-        }
+        const expectedOutput = fs.readFileSync(path.join(__dirname, fixturesDir, fixture, 'expected', 'index.js')).toString();
 
+        const compiler = webpack(config);
+        compiler.outputFileSystem = new MemoryFileSystem();
+
+        compiler.run((err, stats) => {
+          if (err) {
+            reject(err);
+          }
+          const actualOutput = stats.compilation.assets['index.js'].source();
+          expect(actualOutput).to.equal(expectedOutput);
+          resolve();
+        });
       });
-
     });
 
     return Promise.all(promises);
-
   });
-
 });
